bb7de2e7b7474120d03d1f9490e00860ade0a998
Nasim <nasimnu14@gmail.com>
2020-06-11 11:21:40 +0600

    initializes with two candidate

---
+++ contracts/Election.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint votecount;
+    }
+    mapping(uint=> Candidate) public candidates;
+
+    uint public candidatesCount;
+
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        
+    }
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
