3f40e48afd26b055e0159423de504d390f51183c
Nasim <nasimnu14@gmail.com>
2020-06-11 15:36:45 +0600

    metamask is running

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,8 @@ App = {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
+      ethereum.enable();
+
     } else {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
@@ -58,7 +60,7 @@ App = {
     var content = $("#content");
 
     loader.show();
-    content.hide();
+    content.show();
 
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
